b22e84472bdb32eea9ffb6b7cd9a1ad04f6cf557
Jean Loberg Hansen <jean@lait.dk>
2021-05-16 21:28:22 +0200

    preview entrance page

--- queries/coachingEntranceItem.ts
+++ queries/coachingEntranceItem.ts
@@ -1,7 +1,7 @@
 import { contentAreaCollection, pageHeader, seo } from "./queriesUtils/utils";
 const getDynamicData = `
-query getCoachingEntrance($slug: String!) {
-  pageData: coachingEntranceItemCollection(where: {slug: $slug}, limit: 1) {
+query getCoachingEntrance($slug: String!, $isPreview: Boolean!) {
+  pageData: coachingEntranceItemCollection(where: {slug: $slug}, limit: 1, preview:$isPreview) {
     items {
       ${pageHeader}
       coachingProcessTitle {
