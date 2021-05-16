e688bfed704d86fda6312ae33ac0cdd8b7879cd3
Jean Loberg Hansen <jean@lait.dk>
2021-05-16 22:08:42 +0200

    fix content

--- components/gridBlocks/textBlock/textBlock.ts
+++ components/gridBlocks/textBlock/textBlock.ts
@@ -7,9 +7,11 @@ import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 export default class TextBlock extends Vue {
 	@Prop({type: Object as () => ITextBlock, required: true})
 	readonly blockData!: ITextBlock;
-	public bodytext: object | null = this.blockData.bodytext || null;
 
 	public toHtmlString(content: any) {
 		return documentToHtmlString(content);
+	}
+	mounted(){
+		console.debug("blockData", this.blockData);
 	}
 }
--- components/gridBlocks/textBlock/textBlock.vue
+++ components/gridBlocks/textBlock/textBlock.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="grid-r">
 		<div class="grid-c-10-offset-2 grid-remove-offset-sm">
-			<div v-if="bodytext" v-html="toHtmlString(bodytext.json)"></div>
+			<h3 v-if="blockData.title">{{ blockData.title }}</h3>
+			<div v-if="blockData.bodytext" v-html="toHtmlString(blockData.bodytext.json)"></div>
 		</div>
 	</div>
 </template>
