91053ed52d1b030a9dde6bd082599e79853f4c22
Jean Loberg Hansen <jean@lait.dk>
2021-06-26 13:20:36 +0200

    sdfdsf

--- pages/ContentPage/_slug.vue
+++ pages/ContentPage/_slug.vue
@@ -6,8 +6,8 @@
 			:heroImage="data.image"
 			:heroSubtitle="data.subtitle"
 		/>
-		<GridBlocks v-if="data.contentAreaCollection.items.length > 0" :gridContent="data.contentAreaCollection" />
-			<LazyCollaboratorsComponent v-if="data.showCollaborators" />
+		<LazyGridBlocks v-if="data.contentAreaCollection.items.length > 0" :gridContent="data.contentAreaCollection" />
+		<LazyCollaboratorsComponent v-if="data.showCollaborators" />
 	</main>
 </template>
 <script src="./_slug.ts"></script>
