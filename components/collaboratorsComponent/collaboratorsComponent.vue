3a79c3a52a9f395029fc1ae43d4141a522fd1e6c
Jean Loberg Hansen <jean@lait.dk>
2021-07-02 21:29:12 +0200

    slider and null check

--- components/collaboratorsComponent/collaboratorsComponent.vue
+++ components/collaboratorsComponent/collaboratorsComponent.vue
@@ -1,15 +1,10 @@
 <template>
 	<div class="collaborators" v-if="!$fetchState.pending">
-	
 		<div class="grid-r">
 			<div class="grid-c-13-offset-3 grid-remove-offset-sm">
 				<h2 class="collaborators__title">Samarbejdspartnere</h2>
 				<ul v-if="blockData.total < 3" class="collaborators__list reset-ul grid-r">
-					<li
-						v-for="(item, index) in blockData.items"
-						:key="index"
-						class="collaborators__list-item grid-c-4"
-					>
+					<li v-for="(item, index) in blockData.items" :key="index" class="collaborators__list-item grid-c-4">
 						<a :href="item.link" v-if="item.link" target="_blank">
 							<nuxt-img
 								class="collaborators__list-img"
@@ -20,15 +15,17 @@
 								:alt="item.image.title"
 							/>
 						</a>
-						<nuxt-img
-							class="collaborators__list-img"
-							v-else
-							loading="lazy"
-							width="250"
-							:src="item.image.url"
-							sizes="sm:100vw xl:50vw"
-							:alt="item.image.title"
-						/>
+						<div v-else>
+							<nuxt-img
+								class="collaborators__list-img"
+								v-if="item.image"
+								loading="lazy"
+								width="250"
+								:src="item.image.url"
+								sizes="sm:100vw xl:50vw"
+								:alt="item.image.title"
+							/>
+						</div>
 					</li>
 				</ul>
 				<div v-else class="collaborators__slider">
@@ -49,15 +46,17 @@
 									:alt="item.image.title"
 								/>
 							</a>
-							<nuxt-img
-								class="collaborators__list-img"
-								v-else
-								loading="lazy"
-								width="250"
-								:src="item.image.url"
-								sizes="sm:100vw xl:50vw"
-								:alt="item.image.title"
-							/>
+							<div v-else>
+								<nuxt-img
+									class="collaborators__list-img"
+									v-if="item.image"
+									loading="lazy"
+									width="250"
+									:src="item.image.url"
+									sizes="sm:100vw xl:50vw"
+									:alt="item.image.title"
+								/>
+							</div>
 						</Slide>
 						<hooper-navigation slot="hooper-addons"></hooper-navigation>
 					</Hooper>
